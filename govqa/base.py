b66a66380fab000a3aa79e4ac955f753cd7da9ce
hannah cushman garland <hannah.cushman@gmail.com>
2023-03-03 09:40:38 -0600

    Add working login

--- govqa/base.py
+++ govqa/base.py
@@ -1,6 +1,3 @@
-"""
-https://stackoverflow.com/questions/54927385/scrape-aspx-form-with-python
-"""
 from datetime import datetime
 from hashlib import md5
 import os
@@ -20,24 +17,10 @@ class GovQA(Scraper):
     }
     """
 
-    @property
-    def BASE_HEADERS(self):
-        return {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0",
-            "Accept-Language": "en-US",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        }
-
     def __init__(self, domain, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.domain = domain
-
-        # Generate a 25-character hash to use as a token for this session
-        self.session_token = md5(
-            datetime.now().isoformat().encode()
-        ).hexdigest()[:24]
 
     def request(self, *args, **kwargs):
         response = super().request(*args, **kwargs)
@@ -50,27 +33,24 @@ class GovQA(Scraper):
 
         return response
 
-    def get_full_url(self, endpoint):
-        return f"{self.domain}/WEBAPP/_rs/(S({self.session_token}))/{endpoint}"
+    def url_from_endpoint(self, endpoint):
+        return f"{self.domain}/WEBAPP/_rs/{endpoint}"
 
     def create_account(self):
         ...
 
     def login(self, username, password):
-        # TODO: Add base headers in request method
-        login_headers = self.BASE_HEADERS.copy()
-        login_headers["Referer"] = self.get_full_url("SupportHome.aspx")
-
-        login_url = self.get_full_url("Login.aspx")
+        headers = {
+            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36",
+        }
 
         response = self.get(
-            login_url,
-            headers=login_headers
+            self.url_from_endpoint("Login.aspx"),
+            headers=login_headers,
+            allow_redirects=True,
         )
 
         tree = lxml.html.fromstring(response.text)
-
-        # TODO: Get sSessionID?
 
         viewstate = tree.xpath("//input[@id='__VIEWSTATE']")[0].value
         viewstategenerator = tree.xpath("//input[@id='__VIEWSTATEGENERATOR']")[
@@ -78,26 +58,25 @@ class GovQA(Scraper):
         ].value
         request_verification_token =  tree.xpath("//input[@name='__RequestVerificationToken']")[0].value
 
-        login_headers["Referer"] = login_url
-        login_headers["Content-Type"] = "application/x-www-form-urlencoded"
-
-        # TODO: Add cookie?
+        headers["content-type"] = "application/x-www-form-urlencoded"
 
         payload = {
+            "__EVENTTARGET": "",
+            "__EVENTARGUMENT": "",
             "__VIEWSTATE": viewstate,
-            "__VIEWSTATEGENERATOR": viewstategenerator,
-            "__EVENTTARGET": None,
-            "__EVENTARGUMENT": None,
             "__RequestVerificationToken": request_verification_token,
             "ASPxFormLayout1$txtUsername": username,
-            "ASPxFormLayout1$txtPassword:": password,
+            "ASPxFormLayout1$txtPassword": password,
             "ASPxFormLayout1$btnLogin": "Submit",
+            "__VIEWSTATEGENERATOR": viewstategenerator,
+            "__VIEWSTATEENCRYPTED": "",
         }
 
-        return client.post(
-            login_url,
-            headers=login_headers,
+        return self.post(
+            response.url,
             data=payload,
+            headers=headers,
+            allow_redirects=True
         )
 
     def reset_password(self):
@@ -124,5 +103,5 @@ if __name__ == "__main__":
 
     response = client.login(os.environ["GOVQA_USERNAME"], os.environ["GOVQA_PASSWORD"])
 
-    import pdb
-    pdb.set_trace()
+    with open("response.html", "w") as initial_response_content:
+        initial_response_content.write(response.content.decode("utf-8"))
