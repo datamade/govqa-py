92e2e6b38face5289a07e4f663d2b78c1909bd8c
Forest Gregg <fgregg@datamade.us>
2023-05-31 16:06:31 -0400

    start extrating form keys

--- govqa/input_types.py
+++ govqa/input_types.py
@@ -10,27 +10,36 @@ class Input:
 
         self.label = label_element.text.strip(": ").lower().replace(" ", "_")
         self.properties = {"type": "string"}
+        self._form_keys = self._extract_form_keys(table)
 
-    def __str__(self):
-        return self.label
+    def _input_element(self, table):
+        return table.xpath(".//input[not(@type='hidden')]")[0]
+
+    def _extract_form_keys(self, table):
+        input_element = self._input_element(table)
+        return [input_element.attrib["name"]]
 
 
 class TextArea(Input):
-    ...
+    def _input_element(self, table):
+        return table.xpath(".//textarea")[0]
 
 
 class ConstrainedInput(Input):
-    def __init__(self, tree, source_text):
-        super().__init__(tree, source_text)
-        self._valid_values(tree, source_text)
+    def __init__(self, table, source_text):
+        super().__init__(table, source_text)
+        self._valid_values(table, source_text)
 
-    def _valid_values(self, tree, source_text):
+    def _valid_values(self, table, source_text):
         ...
 
 
 class RadioGroup(ConstrainedInput):
+    def _input_element(self, table):
+        return table.xpath(".//table[@role='radiogroup']//input")[0]
+
     def _valid_values(self, table, source_text):
-        input_element = table.xpath(".//table[@role='radiogroup']//input")[0]
+        input_element = self._input_element(table)
         identifier = input_element.attrib["name"]
         line_pattern = rf"'uniqueID':'{re.escape(identifier)}'.*"
         (line,) = re.findall(line_pattern, source_text)
@@ -42,8 +51,12 @@ class RadioGroup(ConstrainedInput):
 
 
 class ComboBox(ConstrainedInput):
+    def _input_element(self, table):
+        return table.xpath(".//input[@role='combobox']")[0]
+
     def _valid_values(self, table, source_text):
-        (input_element,) = table.xpath(".//input[@role='combobox']")
+        input_element = self._input_element(table)
+
         identifier = input_element.name
         line_pattern = rf"'uniqueID':'{re.escape(identifier)}\$DDD\$L'.*"
         (line,) = re.findall(line_pattern, source_text)
@@ -52,6 +65,11 @@ class ComboBox(ConstrainedInput):
         matches = re.search(option_pattern, line)
         options = ast.literal_eval(matches.group(1))
         self.properties["enum"] = [option["value"] for option in options[1:]]
+
+    def _extract_form_keys(self, table):
+        input_element = self._input_element(table)
+        hidden_element = table.xpath(".//input[@type='hidden']")[0]
+        return [input_element.name, hidden_element.name]
 
 
 class CheckBox(ConstrainedInput):
